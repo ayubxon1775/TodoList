6906a1f9ba83c70f7c9be78df8a374d7b1d9ec9d
Ayubxon <ayubxonaxmatxonov@gmail.com>
2025-02-27 17:22:43 +0500

    added code

--- js/app.js
+++ js/app.js
@@ -11,3 +11,37 @@ const hourEl = document.getElementById('hour')
 const minuteEl = document.getElementById('minute')
 const secondEl = document.getElementById('second')
 const closeEl = document.getElementById('close')
+
+// check
+let todos = JSON.parse(localStorage.getItem('list'))
+ ? JSON.parse(localStorage.getItem('list')) 
+ : []
+
+//  setTodos to Localstorage
+function setTodos () {
+  localStorage.setItem('list', JSON.stringify(todos))
+}
+// Show todos 
+
+
+//  Show error
+ function showMessage (where, message) {
+  document.getElementById(`${where}` ).textContent = message
+
+  setTimeout(() => {
+    document.getElementById(`${where}`).textContent = ''
+  },2500)
+ }
+
+// get Todos
+formCreate.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const todoText = formCreate['input-create'].value.trim()
+  formCreate.reset()
+  if (todoText.length) {
+    todos.push({text: todoText, time: '19:49, 14.07.2022', completed: false })
+    setTodos()
+  }else {
+    showMessage('message-create', 'please, Enter some text...')
+  }
+})
